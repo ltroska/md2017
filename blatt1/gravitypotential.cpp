f08176b53d5449c8d54013cea87eead37e5914fe
Lukas Troska <lukas.troska@gmail.com>
2017-10-20 12:07:40 +0200

    erster Teil

--- blatt1/gravitypotential.cpp
+++ blatt1/gravitypotential.cpp
@@ -2,8 +2,11 @@
 
 real GravityPotential::force(Particle &p, Particle &q)
 {
-    // TODO
-    return 0.0;
+    const auto r = std::sqrt((p.x[0]-q.x[0])*(p.x[0]-q.x[0]) + (p.x[1]-q.x[1])*(p.x[1]-q.x[1]));
+
+    const auto potential = -p.m*q.m / r;
+        
+    return potential;
 }
 
 // vim:set et sts=4 ts=4 sw=4 ai ci cin:
