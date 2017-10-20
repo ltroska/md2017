2160dbefcff7a9ac990ef04a910368911298b021
Lukas Troska <lukas.troska@gmail.com>
2017-10-20 12:19:26 +0200

    berechne Kraefte in force

--- blatt1/gravitypotential.cpp
+++ blatt1/gravitypotential.cpp
@@ -2,10 +2,18 @@
 
 real GravityPotential::force(Particle &p, Particle &q)
 {
-    const auto r = std::sqrt((p.x[0]-q.x[0])*(p.x[0]-q.x[0]) + (p.x[1]-q.x[1])*(p.x[1]-q.x[1]));
+    const auto x_diff = p.x[0]-q.x[0];
+    const auto y_diff = p.x[1]-q.x[1];
+    const auto r_sq = x_diff*x_diff + y_diff*y_diff;
+    const auto r = std::sqrt(r_sq);
 
     const auto potential = -p.m*q.m / r;
-        
+
+    const auto factor = p.m*q.m/(r*r_sq);
+
+    p.F[0] = p.F_old[0] + factor * x_diff;
+    p.F[1] = p.F_old[1] + factor * y_diff;
+
     return potential;
 }
 
